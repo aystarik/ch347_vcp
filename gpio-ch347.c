8365e0bf5032feeccf9fe0bc0aded05bc17d0e6c
Nathan Schulte <nmschulte@desmas.net>
2025-04-29 20:28:05 -0500

    kernel 6.13

--- gpio-ch347.c
+++ gpio-ch347.c
@@ -203,9 +203,9 @@ static int ch347_gpio_probe(struct platform_device *pdev)
 	return 0;
 }
 
-static int ch347_gpio_remove(struct platform_device *pdev)
+static void ch347_gpio_remove(struct platform_device *pdev)
 {
-	return 0;
+	return;
 }
 
 static struct platform_driver ch347_gpio_driver = {
